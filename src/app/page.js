9a262b6b0d5ce543cfd48e6c425cc3edb703b829
JovanJJ <jovanjj99@gmail.com>
2026-02-09 19:57:53 +0100

    Fix title typo on home page.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import Main from "../app/components/Main";
 
 export const metadata = {
-  title: "Soundproof Craetions",
+  title: "Soundproof Creations",
   description: "Soundproof Creations helps improve room acoustics with decorative soundproof panels, curtains, and practical noise-reduction solutions for modern spaces.",
 };
 
